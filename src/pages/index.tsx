1aab2adcc4b9f5603def88c1766f773eea066dd5
justadamhopkins <adamhopkins87@gmail.com>
2020-11-29 21:36:57 +0000

    formatted homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,18 @@
-import * as React from 'react';
-import Link from 'gatsby-link';
+import React, { FC } from 'react'
+import { Link } from 'gatsby'
 
-// Please note that you can use https://github.com/dotansimha/graphql-code-generator
-// to generate all types from graphQL schema
-interface IndexPageProps {
-  data: {
-    site: {
-      siteMetadata: {
-        title: string;
-      };
-    };
-  };
-}
-
-export default class extends React.Component<IndexPageProps, {}> {
-  constructor(props: IndexPageProps, context: any) {
-    super(props, context);
-  }
-  public render() {
-    return (
-      <div>
-        <h1>Hi people</h1>
-        <p>
-          Welcome to your new{' '}
-          <strong>{this.props.data.site.siteMetadata.title}</strong> site.
-        </p>
-        <p>Now go build something great.</p>
-        <Link to='/page-2/'>Go to page 2</Link>
-      </div>
-    );
-  }
+const HomePage: FC<{}> = ({ data }) => {
+  return (
+    <div>
+      <h1>Hi people</h1>
+      <p>
+        Welcome to your new <strong>{data.site.siteMetadata.title}</strong>{' '}
+        site.
+      </p>
+      <p>Now go build something great.</p>
+      <Link to='/page-2/'>Go to page 2</Link>
+    </div>
+  )
 }
 
 export const pageQuery = graphql`
@@ -40,4 +23,6 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
+
+export default HomePage
