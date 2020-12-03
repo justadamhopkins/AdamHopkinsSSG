449a476c226ac9395a8e9ce892d374a610d9b203
justadamhopkins <adamhopkins87@gmail.com>
2020-12-03 15:54:26 +0000

    code refactor

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,11 @@ module.exports = {
   siteMetadata: {
     title: `A portfolio website for Adam Hopkins`,
     siteUrl: 'https://adamhopkins.dev',
+    description: 'A portfolio site for all things Adam Hopkins'
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
-    `gatsby-plugin-styled-components`,
-  ],
+    `gatsby-plugin-styled-components`
+  ]
 }
