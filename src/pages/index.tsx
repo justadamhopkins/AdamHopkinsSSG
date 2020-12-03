79717adb2473b648ff3f7483f6e271c31b380fc9
justadamhopkins <adamhopkins87@gmail.com>
2020-12-03 15:54:18 +0000

    added helmet to index component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,14 @@
 import React, { FC } from 'react'
-import { Link } from 'gatsby'
+import { SiteHelmet } from '../components/SiteHelmet/SiteHelmet'
 
-const HomePage: FC<{}> = ({ data }) => {
+const HomePage: FC<{}> = () => {
   return (
-    <div>
+    <>
+      <SiteHelmet pageTitle='Portfolio home page' />
       <h1>Hi people</h1>
-      <p>
-        Welcome to your new <strong>{data.site.siteMetadata.title}</strong>{' '}
-        site.
-      </p>
       <p>Now go build something great.</p>
-      <Link to='/page-2/'>Go to page 2</Link>
-    </div>
+    </>
   )
 }
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
 
 export default HomePage
