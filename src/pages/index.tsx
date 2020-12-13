c7d9d97eb4de23ec0c913d942b0c0bed5674c40f
justadamhopkins <adamhopkins87@gmail.com>
2020-12-13 20:59:25 +0000

    remove global font styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react'
 import styled from 'styled-components'
 import { SiteHelmet } from '../components/SiteHelmet/SiteHelmet'
-import { GlobalFontStyles } from '../styles/fonts/fonts'
 
 const Header = styled.h1`
   font-family: 'LeagueSpartanBold';
@@ -10,7 +9,6 @@ const Header = styled.h1`
 const HomePage: FC<{}> = () => {
   return (
     <>
-      <GlobalFontStyles />
       <SiteHelmet pageTitle='Portfolio home page' />
       <Header>Hi people</Header>
       <p>Now go build something great.</p>
