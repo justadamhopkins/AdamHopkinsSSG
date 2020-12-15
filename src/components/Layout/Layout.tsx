145a0a42aaac85e9fe46cdf3a45417d4dbbe68e6
justadamhopkins <adamhopkins87@gmail.com>
2020-12-15 16:48:12 +0000

    added site wide margin

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,22 @@
 import React, { FC } from 'react'
-import { GlobalFontStyles } from '../../styles/fonts/fonts'
+import { GlobalFontStyles } from '../../styles/fonts/GlobalFontStyles'
+import { GlobalUiStyles } from '../../styles/ui/GlobalUiStyles'
+import { Nav } from '../Nav/Nav'
+import styled from 'styled-components'
+
+const StyledSiteWrapper = styled.div`
+  margin: 1.5rem;
+`
 
 export const Layout: FC<{}> = ({ children }) => {
   return (
     <>
       <GlobalFontStyles />
-      {children}
+      <GlobalUiStyles />
+      <StyledSiteWrapper>
+        <Nav />
+        {children}
+      </StyledSiteWrapper>
     </>
   )
 }
