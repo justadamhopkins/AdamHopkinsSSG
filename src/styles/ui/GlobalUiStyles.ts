8cdeb9a1f293ddbd32d153e5ab55d860c02654fc
justadamhopkins <adamhopkins87@gmail.com>
2021-01-05 21:38:40 +0000

    added more default css

--- src/styles/ui/GlobalUiStyles.ts
+++ src/styles/ui/GlobalUiStyles.ts
@@ -2,7 +2,13 @@ import { createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset'
 
 export const GlobalUiStyles = createGlobalStyle`
-  ${reset} 
+  ${reset}
+    *, *:before, *:after {
+    box-sizing: border-box;
+  }
+  html {
+    box-sizing: border-box;
+  }
   :root {
     --red: #ff4949;
     --black: #2e2e2e;
@@ -10,7 +16,16 @@ export const GlobalUiStyles = createGlobalStyle`
     --white: #fff;
     --grey: #efefef;
   }
+  html {
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    color: '#1a202c';
+  }
   body {
     font-size: 62.5%;
+    line-height: 1.5;
+    letter-spacing: 0;
+    background-color: '#f7fafc';
   }
 `
