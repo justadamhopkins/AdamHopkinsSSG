c34286bbe285d17875088b3907d2d6dfef24d75b
justadamhopkins <adamhopkins87@gmail.com>
2021-01-05 21:37:59 +0000

    updated ssr with wrapping root element

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Layout } from './src/components/Layout/Layout'
 
-export function wrapPageElement({ element, props }) {
+export function wrapRootElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>
 }
