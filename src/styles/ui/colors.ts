2294399a904ae213d400bfdb60546f1af57a3cbb
justadamhopkins <adamhopkins87@gmail.com>
2021-01-05 21:41:27 +0000

    added colors

---
+++ src/styles/ui/colors.ts
@@ -0,0 +1,19 @@
+const darkPalette = {
+  white: '#fff',
+  black: '#000',
+  materialBlack: '#282c35',
+  darkGrey: '#464655',
+  darkPurps: '#686276',
+  darkLilac: '#907e96',
+  lightLilac: '#bb9bb5',
+  darkPink: '#e8b9d2',
+  lightGrey: '#c0c5d1'
+}
+
+const lightPalette = {
+  smoothPurple: '#673ab7',
+  smoothGrey: '#b0a8b9',
+  smoothGreen: '#00b397',
+  white: '#fff',
+  black: '#000'
+}
