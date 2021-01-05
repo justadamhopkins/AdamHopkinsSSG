9d129a7f790e384a30e5dea26467909faec7c5e2
justadamhopkins <adamhopkins87@gmail.com>
2021-01-05 21:39:24 +0000

    created hamburger component

---
+++ src/components/Hamburger/Hamburger.tsx
@@ -0,0 +1,74 @@
+import React, { Dispatch, FC, SetStateAction } from 'react'
+import styled, { css } from 'styled-components'
+
+const StyledHamburger = styled.div<{ active: boolean }>`
+  span {
+    width: 40px;
+    height: 4px;
+    background-color: red;
+    display: block;
+    margin: 8px auto;
+    -webkit-transition: all 0.3s ease-in-out;
+    -o-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
+  }
+  ${({ active }) =>
+    active &&
+    css`
+      -webkit-transition: all 0.3s ease-in-out;
+      -o-transition: all 0.3s ease-in-out;
+      transition: all 0.3s ease-in-out;
+      -webkit-transition-delay: 0.6s;
+      -o-transition-delay: 0.6s;
+      transition-delay: 0.6s;
+      -webkit-transform: rotate(45deg);
+      -ms-transform: rotate(45deg);
+      -o-transform: rotate(45deg);
+      transform: rotate(45deg);
+      span {
+        :nth-child(2) {
+          width: 0px;
+        }
+        :nth-child(1),
+        :nth-child(3) {
+          -webkit-transition-delay: 0.3s;
+          -o-transition-delay: 0.3s;
+          transition-delay: 0.3s;
+        }
+        :nth-child(1) {
+          -webkit-transform: translateY(13px);
+          -ms-transform: translateY(13px);
+          -o-transform: translateY(13px);
+          transform: translateY(13px);
+        }
+        :nth-child(3) {
+          -webkit-transform: translateY(-13px) rotate(90deg);
+          -ms-transform: translateY(-13px) rotate(90deg);
+          -o-transform: translateY(-13px) rotate(90deg);
+          transform: translateY(-13px) rotate(90deg);
+        }
+      }
+    `};
+  :hover {
+    cursor: pointer;
+  }
+`
+
+type Props = {
+  active: boolean
+  setToggleHamburger: Dispatch<SetStateAction<boolean>>
+}
+
+export const Hamburger: FC<Props> = ({ active, setToggleHamburger }) => {
+  return (
+    <StyledHamburger
+      active={active}
+      onClick={() => setToggleHamburger(!active)}
+      data-testid='hamburger'
+    >
+      <span></span>
+      <span></span>
+      <span></span>
+    </StyledHamburger>
+  )
+}
---
+++ src/components/Hamburger/__tests__/Hamburger.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { screen, render, fireEvent } from '@testing-library/react'
+import { Hamburger } from '../Hamburger'
+
+describe('Hamburger', () => {
+  beforeEach(() => jest.clearAllMocks())
+  test('renders Hamburger', () => {
+    render(<Hamburger active={false} setToggleHamburger={jest.fn()} />)
+
+    expect(screen.getByTestId('hamburger')).toBeTruthy()
+  })
+  test('handles onClick', () => {
+    const spy = jest.fn()
+
+    render(<Hamburger active={false} setToggleHamburger={spy} />)
+
+    fireEvent.click(screen.getByTestId('hamburger'))
+
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+})
