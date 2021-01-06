b9584eb992af92bcf86da99e865a0d065e937e04
justadamhopkins <adamhopkins87@gmail.com>
2021-01-06 21:59:41 +0000

    added more colors to root

--- src/styles/ui/GlobalUiStyles.ts
+++ src/styles/ui/GlobalUiStyles.ts
@@ -8,6 +8,16 @@ export const GlobalUiStyles = createGlobalStyle`
     --yellow: #ffc600;
     --white: #fff;
     --grey: #efefef;
+    --materialBlack: #282c35;
+    --darkGrey: #464655;
+    --darkPurps: #686276;
+    --darkLilac: #907e96;
+    --lightLilac: #bb9bb5;
+    --darkPink: #e8b9d2;
+    --lightGrey: #c0c5d1;
+    --smoothPurple: #673ab7;
+    --smoothGrey: #b0a8b9;
+    --smoothGreen: #00b397;
   }
   ${reset}
   *, *:before, *:after {
