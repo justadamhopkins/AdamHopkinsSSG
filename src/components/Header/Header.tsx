3cec5c006785937d9c6b7ad3facfe86d18dc59ef
justadamhopkins <adamhopkins87@gmail.com>
2021-01-13 23:55:51 +0000

    added styles passed props

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { Dispatch, FC, SetStateAction, useState } from 'react'
 import styled from 'styled-components'
 import { Hamburger } from '../Hamburger/Hamburger'
 import { Nav } from '../Nav/Nav'
@@ -6,15 +6,25 @@ import { Nav } from '../Nav/Nav'
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
+  flex-direction: row;
+  div:nth-child(1) {
+    align-self: center;
+    text-align: center;
+    flex-grow: 1;
+  }
 `
 
-export const Header: FC = () => {
+interface Props {
+  toggleTheme(): void
+}
+
+export const Header: FC<Props> = ({ toggleTheme }) => {
   const [isNavOpen, setisNavOpen] = useState(false)
 
   return (
     <>
       <Wrapper>
-        <div>test</div>
+        <div onClick={toggleTheme}>click me</div>
         <Hamburger active={isNavOpen} toggleHamburger={setisNavOpen} />
       </Wrapper>
       <Nav isNavOpen={isNavOpen} toggleNav={setisNavOpen} />
