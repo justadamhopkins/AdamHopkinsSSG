8187cc0982c8a7611143c4bb035df2e5ce0223cf
justadamhopkins <adamhopkins87@gmail.com>
2021-01-29 22:52:35 +0000

    added footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,24 +4,26 @@ import { GlobalUiStyles } from '../../styles/ui/GlobalUiStyles'
 import { Header } from '../Header/Header'
 import styled from 'styled-components'
 import { useThemeSwitcher } from '../../hooks/useThemeSwitcher'
+import { Footer } from '../Footer/Footer'
 
 const StyledSiteWrapper = styled.div`
-  padding: 2rem;
   background-color: var(--background);
-  color: var(--dog);
+  height: calc(100vh - 4rem);
+  padding: 2rem;
 `
 
 export const Layout: FC<{}> = ({ children }) => {
   const { toggleTheme, theme } = useThemeSwitcher()
 
   return (
-    <>
+    <div className={theme}>
       <GlobalFontStyles />
       <GlobalUiStyles />
-      <StyledSiteWrapper className={theme}>
+      <StyledSiteWrapper>
         <Header toggleTheme={toggleTheme} />
         {children}
       </StyledSiteWrapper>
-    </>
+      <Footer />
+    </div>
   )
 }
