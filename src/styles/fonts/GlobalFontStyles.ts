91e5dd65e9a3622fa1c034e0b790839048dd988c
justadamhopkins <adamhopkins87@gmail.com>
2021-02-05 18:30:39 +0000

    added font missing otf

--- src/styles/fonts/GlobalFontStyles.ts
+++ src/styles/fonts/GlobalFontStyles.ts
@@ -3,8 +3,17 @@ import { createGlobalStyle } from 'styled-components'
 import LeagueSpartanBoldWoff from './LeagueSpartanBold.woff' //@ts-ignore
 import LeagueSpartanBoldWoff2 from './LeagueSpartanBold.woff2' //@ts-ignore
 import LeagueSpartanBoldOtf from './LeagueSpartanBold.otf' //@ts-ignore
-
+import FiraCodeLightWoff from './FiraCode.woff' //@ts-ignore
+import FiraCodeLightWoff2 from './FiraCode.woff2' //@ts-ignore
 export const GlobalFontStyles = createGlobalStyle`
+    @font-face {
+        font-family: 'FiraCodeLight';
+        src: local('FiraCodeLight'), local('FiraCodeLight'),
+        url(${FiraCodeLightWoff}) format('woff2'),
+        url(${FiraCodeLightWoff2}) format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
     @font-face {
         font-family: 'LeagueSpartanBold';
         src: local('LeagueSpartanBold'), local('LeagueSpartanBold'),
