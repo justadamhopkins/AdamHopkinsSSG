85d6e9ed69a3335098e1e9171959b503429682d2
justadamhopkins <adamhopkins87@gmail.com>
2021-02-06 14:53:56 +0000

    fix: flash of content

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Layout } from './src/components/Layout/Layout'
 
-export function wrapRootElement({ element, props }) {
+export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>
 }
