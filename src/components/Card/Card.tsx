be7b41afb6a0205c8ba4027e8fadc465616b77f3
justadamhopkins <adamhopkins87@gmail.com>
2021-02-24 22:38:05 +0000

    started card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledCardContentWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: block;
+  pointer-events: none;
+  > div {
+    pointer-events: auto;
+    position: relative;
+    border-radius: 20px;
+    background: #1c1c1e;
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+  }
+`
 
 export const Card: React.FC = () => {
-  return <div>card</div>
+  return (
+    <StyledCardContentWrapper>
+      <div>Card</div>
+    </StyledCardContentWrapper>
+  )
 }
