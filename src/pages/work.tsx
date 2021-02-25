e0089e8dcceecc5134062d822aaa6d31c6def5e5
justadamhopkins <adamhopkins87@gmail.com>
2021-02-25 20:04:53 +0000

    tidy work file

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react'
 import styled from 'styled-components'
 import { SiteHelmet } from '../components/SiteHelmet/SiteHelmet'
 import { Title } from '../components/Title/Title'
-import { Card } from '../components/Card/Card'
+import { CardList } from '../components/Card/Card'
 
 const StyledWorkWrapper = styled.section`
   text-align: center;
@@ -12,29 +12,6 @@ const StyledWorkWrapper = styled.section`
 
 const StyledCardWrapper = styled.div`
   padding-top: 3rem;
-  ul {
-    display: flex;
-    flex-wrap: wrap;
-    align-content: flex-start;
-    li {
-      position: relative;
-      padding: 25px;
-      height: 350px;
-      flex: 0 0 40%;
-      max-width: 40%;
-      &:nth-child(4n + 1),
-      &:nth-child(4n + 4) {
-        flex: 0 0 60%;
-        max-width: 60%;
-      }
-      &:nth-child(odd) {
-        padding-left: 0;
-      }
-      &:nth-child(even) {
-        padding-right: 0;
-      }
-    }
-  }
 `
 
 const WorkPage: FC<{}> = () => {
@@ -44,14 +21,7 @@ const WorkPage: FC<{}> = () => {
       <StyledWorkWrapper>
         <Title delay={2}>My Portfolio</Title>
         <StyledCardWrapper>
-          <ul>
-            <li>
-              <Card />
-            </li>
-            <li>
-              <Card />
-            </li>
-          </ul>
+          <CardList />
         </StyledCardWrapper>
       </StyledWorkWrapper>
     </>
