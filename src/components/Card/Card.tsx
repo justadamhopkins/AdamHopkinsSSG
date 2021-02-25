86ebab47a24c243b69c1673de69c6c3fdb0de6c8
justadamhopkins <adamhopkins87@gmail.com>
2021-02-25 20:05:02 +0000

    added animation to card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { motion, AnimateSharedLayout, AnimatePresence } from 'framer-motion'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 const StyledCardContentWrapper = styled.div`
@@ -19,10 +20,65 @@ const StyledCardContentWrapper = styled.div`
   }
 `
 
-export const Card: React.FC = () => {
+const StyledCardList = styled(motion.ul)`
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  li {
+    position: relative;
+    padding: 25px;
+    height: 350px;
+    flex: 0 0 40%;
+    max-width: 40%;
+    &:nth-child(4n + 1),
+    &:nth-child(4n + 4) {
+      flex: 0 0 60%;
+      max-width: 60%;
+    }
+    &:nth-child(odd) {
+      padding-left: 0;
+    }
+    &:nth-child(even) {
+      padding-right: 0;
+    }
+  }
+`
+
+const Card: React.FC = () => {
   return (
     <StyledCardContentWrapper>
-      <div>Card</div>
+      <Content />
     </StyledCardContentWrapper>
+  )
+}
+
+const Content = () => {
+  return (
+    <motion.div
+      layout
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <div>dog</div>
+      <div>dog</div>
+      <div>dog</div>
+    </motion.div>
+  )
+}
+
+export const CardList = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleOpen = () => setIsOpen(!isOpen)
+  return (
+    <AnimateSharedLayout>
+      <StyledCardList layout>
+        <motion.li layout onClick={toggleOpen}>
+          <AnimatePresence>{isOpen && <Content />}</AnimatePresence>
+          <Card />
+        </motion.li>
+      </StyledCardList>
+    </AnimateSharedLayout>
   )
 }
