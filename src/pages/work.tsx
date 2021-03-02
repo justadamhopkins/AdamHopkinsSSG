171d8b1cb87daa548bbcae9f5b7b326465aa8ddf
justadamhopkins <adamhopkins87@gmail.com>
2021-03-02 22:37:02 +0000

    edited work page

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,8 +1,10 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import styled from 'styled-components'
 import { SiteHelmet } from '../components/SiteHelmet/SiteHelmet'
 import { Title } from '../components/Title/Title'
 import { CardList } from '../components/Card/Card'
+import { Modal } from '../components/Modal/Modal'
+import { portfolioDescriptions } from '../utils/portfolioData/portfolioDescriptions'
 
 const StyledWorkWrapper = styled.section`
   text-align: center;
@@ -14,15 +16,22 @@ const StyledCardWrapper = styled.div`
   padding-top: 3rem;
 `
 
+export type ModalState = { id: string | null; isModalOpen: boolean }
+
 const WorkPage: FC<{}> = () => {
+  const [modalState, setModalState] = useState({ isModalOpen: false, id: null })
+
   return (
     <>
-      <SiteHelmet pageTitle='Portfolio work page' />
+      <SiteHelmet pageTitle='Portfolio' />
       <StyledWorkWrapper>
-        <Title delay={2}>My Portfolio</Title>
+        <Title delay={2}>Portfolio</Title>
         <StyledCardWrapper>
-          <CardList />
+          <CardList setModalState={setModalState} />
         </StyledCardWrapper>
+        <Modal modalState={modalState} setModalState={setModalState}>
+          <div>{portfolioDescriptions[modalState.id]}</div>
+        </Modal>
       </StyledWorkWrapper>
     </>
   )
