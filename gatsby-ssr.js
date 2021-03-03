8811daff16aa79d82ec4e20c6c7e411b9eb0a504
justadamhopkins <adamhopkins87@gmail.com>
2021-03-03 17:29:46 +0000

    added context to ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Layout } from './src/components/Layout/Layout'
+import { ModalProvider } from './src/contexts/ModalContext'
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>
+}
+
+export function wrapRootElement({ element }) {
+  return <ModalProvider>{element}</ModalProvider>
 }
