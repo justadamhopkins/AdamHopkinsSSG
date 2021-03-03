d527584b0537117c9b1a27ac3ff503cadfa8ad0c
justadamhopkins <adamhopkins87@gmail.com>
2021-03-03 17:29:10 +0000

    added modal context

---
+++ src/contexts/ModalContext.tsx
@@ -0,0 +1,43 @@
+import React, { createContext, FC, useContext, useState } from 'react'
+import { Modal } from '../components/Modal/Modal'
+
+interface ModalContextInitialState {
+  closeModal(): void
+  triggerModal(content: string): void
+  modalContent: string | null
+  showModal: boolean
+}
+
+const ModalContext = createContext<ModalContextInitialState>({
+  closeModal: () => {},
+  triggerModal: (content: string) => content,
+  modalContent: null,
+  showModal: false
+})
+
+export const ModalProvider: FC = ({ children }) => {
+  const [showModal, setShowModal] = useState(false)
+  const [modalContent, setModalContent] = useState(null)
+
+  const closeModal = () => {
+    setShowModal(false)
+    setModalContent('')
+  }
+
+  const triggerModal = (content: string) => {
+    setShowModal(!showModal)
+    setModalContent(content)
+  }
+
+  return (
+    <ModalContext.Provider
+      value={{ closeModal, triggerModal, modalContent, showModal }}
+    >
+      <Modal />
+      {children}
+    </ModalContext.Provider>
+  )
+}
+
+export const useModalConfig = () =>
+  useContext(ModalContext) as ModalContextInitialState
