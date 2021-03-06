2120938583b0f26c2d132d5c7bc56fb7c92347a4
justadamhopkins <adamhopkins87@gmail.com>
2021-03-06 22:58:15 +0000

    added merge image function

--- src/utils/mergePortfolioData.ts
+++ src/utils/mergePortfolioData.ts
@@ -1,10 +1,8 @@
-export const mergePortfolioData = (portfolios, images) => {
-  return portfolios.reduce((acc, portfolio) => {
-    const portfolioImage = images.allImageSharp.nodes.find(image => {
+export const mergePortfolioData = (portfolios: any, images: any) => {
+  return portfolios.reduce((acc: any, portfolio: any) => {
+    const portfolioImage = images.allImageSharp.nodes.find((image: any) => {
       return image.fluid.originalName.includes(portfolio.id)
     })
-
-    console.log(portfolioImage, 'portfolioImage')
 
     return [...acc, { ...portfolio, ...portfolioImage }]
   }, [])
