618a3c9b1842c8185341bc98e004dfca36862eaa
justadamhopkins <adamhopkins87@gmail.com>
2021-03-06 22:58:36 +0000

    refactored portfolio page to merge image data into portfolio data

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -5,12 +5,14 @@ import { SiteHelmet } from '../components/SiteHelmet/SiteHelmet'
 import { Title } from '../components/Title/Title'
 import { CardList } from '../components/Card/Card'
 import { useModalConfig } from '../contexts/ModalContext'
+import { PageProps } from 'gatsby'
+import { mergePortfolioData } from '../utils/mergePortfolioData'
 
 const portfolioHistory = [
-  { title: 'Times Puzzles', id: 'job1', content: 'job1content' },
-  { title: 'Times Cancellation', id: 'job2', content: 'job2content' },
-  { title: 'Times Help', id: 'job3', content: 'job3content' },
-  { title: 'Topshop', id: 'job4', content: 'job4content' }
+  { title: 'Times Puzzles', id: 'timesPuzzles', content: 'job1content' },
+  { title: 'Times Cancellation', id: 'cancellation', content: 'job2content' },
+  { title: 'Times Help', id: 'help', content: 'job3content' },
+  { title: 'Topshop', id: 'topshop', content: 'job4content' }
 ]
 
 const StyledWorkWrapper = styled.section`
@@ -23,9 +25,10 @@ const StyledCardWrapper = styled.div`
   padding-top: 3rem;
 `
 
-const PortfolioPage: FC<{}> = ({ data }) => {
-  console.log(data)
+const PortfolioPage: FC<PageProps> = ({ data }) => {
   const { triggerModal } = useModalConfig()
+
+  const mergedPortfolios = mergePortfolioData(portfolioHistory, data)
 
   return (
     <>
@@ -34,7 +37,7 @@ const PortfolioPage: FC<{}> = ({ data }) => {
         <Title delay={2}>Portfolio</Title>
         <StyledCardWrapper>
           <CardList
-            portfolioHistory={portfolioHistory}
+            portfolioHistory={mergedPortfolios}
             triggerModal={triggerModal}
           />
         </StyledCardWrapper>
@@ -49,8 +52,9 @@ export const query = graphql`
   query PortfolioImageQuery {
     allImageSharp {
       nodes {
-        fixed(height: 700) {
-          ...GatsbyImageSharpFixed
+        fluid(maxHeight: 350) {
+          ...GatsbyImageSharpFluid
+          originalName
         }
       }
     }
