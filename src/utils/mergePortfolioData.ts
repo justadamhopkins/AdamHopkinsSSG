95487aac47a640b0d05f686fe0ba1d5631017d5d
justadamhopkins <adamhopkins87@gmail.com>
2021-03-08 14:49:01 +0000

    fixed merge data

--- src/utils/mergePortfolioData.ts
+++ src/utils/mergePortfolioData.ts
@@ -1,7 +1,7 @@
 export const mergePortfolioData = (portfolios: any, images: any) => {
   return portfolios.reduce((acc: any, portfolio: any) => {
     const portfolioImage = images.allImageSharp.nodes.find((image: any) => {
-      return image.fluid.originalName.includes(portfolio.id)
+      return image.fixed.originalName.includes(portfolio.id)
     })
 
     return [...acc, { ...portfolio, ...portfolioImage }]
