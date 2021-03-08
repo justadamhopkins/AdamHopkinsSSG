fc9a5ef540007a48f09bf0377dcd20031cd9f6d0
justadamhopkins <adamhopkins87@gmail.com>
2021-03-08 14:49:46 +0000

    changed z index on modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -10,7 +10,7 @@ const Overlay = styled(motion.div)`
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.3);
-  z-index: 1;
+  z-index: 2;
 `
 const ModalContainer = styled(motion.div)`
   width: 50%;
