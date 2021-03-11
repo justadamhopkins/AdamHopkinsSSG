f4705bd7584d3e7aca0a930e6a55c7b3ba8093a8
justadamhopkins <adamhopkins87@gmail.com>
2021-03-11 22:41:10 +0000

    added work content

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -9,10 +9,30 @@ import { PageProps } from 'gatsby'
 import { mergePortfolioData } from '../utils/mergePortfolioData'
 
 const portfolioHistory = [
-  { title: 'Times Puzzles', id: 'timesPuzzles', content: 'job1content' },
-  { title: 'Times Cancellation', id: 'cancellation', content: 'job2content' },
-  { title: 'Times Help', id: 'help', content: 'job3content' },
-  { title: 'Topshop', id: 'topshop', content: 'job4content' }
+  {
+    title: 'Times Puzzles',
+    id: 'timesPuzzles',
+    content:
+      'I developed a React Native Typescript app for The Times. This was launched to both the app and play store in January 2021.'
+  },
+  {
+    title: 'Times Cancellation',
+    id: 'cancellation',
+    content:
+      'I developed an online subscription cancellation journey for The Times. This was built using React, Apollo client, Typescript, React testing library and Cypress. A GraphQL api was also developed using Apollo server to integrate and consume a rest api.'
+  },
+  {
+    title: 'Times Help',
+    id: 'help',
+    content:
+      'I developed a new help application for The Times. This was built using React, Typescript, React testing library and Cypress. I also developed a serverless lambda that integrated with a service called Algolia for fetching and storing of help articles.'
+  },
+  {
+    title: 'Topshop',
+    id: 'topshop',
+    content:
+      'I worked on a re-platform for Arcadia group. I redeveloped their 8 brand e-commerce websites in React, Node, Enzyme and Cypress.'
+  }
 ]
 
 const StyledWorkWrapper = styled.section`
